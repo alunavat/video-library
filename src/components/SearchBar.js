e2d18a8368784b1a11e142c6bebd570267c27159
Anmol Lunavat <anmol_lunavat@dell.com>
2020-06-04 15:46:25 +0530

    Commit with final code

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui from">
+          <div className="field">
+            <label>Image Search</label>
+            <br></br>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+            {/* Do not put () for callback function */}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
